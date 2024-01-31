1c855530dcea7efa705112c47d28ae9879b0b355
korpaxdev <korpaxdev@gmail.com>
2024-01-31 16:19:33 +0300

    add custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="users/avatar/%Y/%m/%d", blank=True, verbose_name="Аватар")
+
+    def __str__(self):
+        return self.username
