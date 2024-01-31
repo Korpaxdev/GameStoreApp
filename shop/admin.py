c08dac7c26df665918efceb855a35fde253e2876
korpaxdev <korpaxdev@gmail.com>
2024-01-31 17:37:21 +0300

    add category model in shop add

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from shop.models import Category
+
+
 # Register your models here.
+
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    readonly_fields = ("slug",)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from transliterate import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Slug")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name, "ru")
+        super().save()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
